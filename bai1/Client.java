beef1464207d0985827870ed25f6ef710773dfb8
kynguyen12 <56568079+kynguyen12@users.noreply.github.com>
2020-10-26 23:46:06 -0700

    lab4

---
+++ bai1/Client.java
@@ -0,0 +1,30 @@
+package bai1;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class Client {
+
+	public static void main(String[] args) {
+		try {
+			Socket socket = new Socket("localhost",8885);
+			DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+			DataInputStream in = new DataInputStream(socket.getInputStream());
+			Scanner input = new Scanner(System.in);
+			while(true) {
+				System.out.println("\nI: ");
+				out.writeUTF(input.nextLine());
+				out.flush();
+				String str =in.readUTF();
+				System.out.println("Server: " + str);
+			}
+		} catch (IOException e) {
+			System.out.println("ket thuc");
+		}
+	}
+
+}
